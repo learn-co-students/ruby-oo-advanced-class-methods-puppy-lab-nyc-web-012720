9e1fc97b1c4da3706706483d9136444a89b52fe2
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-02-02 14:55:03 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        self.all.map do |dog|
+            puts dog.name
+        end
+    end
+
+    def save #this is save vs self.save, because it needs to be an instance method not a class method. This instance methode will then be referenced in the class method.            
+        self.class.all << self
+    end
+end
