bd321adf8c6be9dc4256f6fd06fef149d715d655
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-02-02 15:02:12 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,7 +15,7 @@ class Dog
     end
 
     def self.clear_all
-        self.all.clear
+        self.all.clear #this is a class method so self is the class
     end
 
     def self.print_all
@@ -25,6 +25,6 @@ class Dog
     end
 
     def save #this is save vs self.save, because it needs to be an instance method not a class method. This instance methode will then be referenced in the class method.            
-        self.class.all << self
+        self.class.all << self #This is an instance method so self is an instance.
     end
 end
